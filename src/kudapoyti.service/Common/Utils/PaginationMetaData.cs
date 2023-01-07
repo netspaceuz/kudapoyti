3c0061f6ea709849c19ef7c5538fdfe229f3a807
Zulaykho Saparbaeva <saparbaevazulaykho18@gmail.com>
2023-01-08 01:46:00 +0500

    solved

--- src/kudapoyti.service/Common/Utils/PaginationMetaData.cs
+++ src/kudapoyti.service/Common/Utils/PaginationMetaData.cs
@@ -8,13 +8,10 @@ namespace kudapoyti.Service.Common.Utils;
 
 public class PaginationMetaData
 {
-    public class PaginationMetaData
-    {
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public bool HasNext { get; set; }
         public bool HasPrevious { get; set; }
         public int PageSize { get; set; }
         public int TotalItems { get; set; }
-    }
 }
